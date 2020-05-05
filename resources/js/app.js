3dddf8fdab9f526a21134eebdc21e2c9e0f3b6dc
Shaiful Islam <kuvic16@gmail.com>
2020-05-05 15:10:20 +0600

    smooth scrolling component and usage

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import routes from "./routes";
 import axios from "axios";
+import ScrollLink from "./components/ScrollLink";
 
 window.axios = axios;
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 Vue.use(VueRouter);
 
+Vue.component("scroll-link", ScrollLink);
 let app = new Vue({
     el: "#app",
     router: new VueRouter(routes)
